7eacec09211efbb3565f5e029f4eb24bd385a76b
Scott <scott.schultz@jotunga.com>
2024-08-01 20:22:58 +1000

    Basic blazor frontend to display feeds

--- Nulah.RSS.App/Program.cs
+++ Nulah.RSS.App/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Nulah.RSS.App.Services;
 
 namespace Nulah.RSS.App;
 
@@ -7,11 +8,19 @@ public class Program
 {
 	public static async Task Main(string[] args)
 	{
+		// Default blazor hookups
 		var builder = WebAssemblyHostBuilder.CreateDefault(args);
 		builder.RootComponents.Add<App>("#app");
 		builder.RootComponents.Add<HeadOutlet>("head::after");
 
-		builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+		// HttpClient to talk to the backend Api
+		// TODO: maybe make this keyed/bound to a particular type for the FeedService
+		builder.Services.AddScoped(sp =>
+			// TODO: change this to configuration, for now its hardcoded to the api https address
+			new HttpClient { BaseAddress = new Uri("https://localhost:7133") }
+		);
+		// Handles interacting with the Api
+		builder.Services.AddScoped<FeedService>();
 
 		await builder.Build().RunAsync();
 	}
---
+++ Nulah.RSS.App/Services/FeedService.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http.Json;
+using Nulah.RSS.Domain.Interfaces;
+using Nulah.RSS.Domain.Models;
+
+namespace Nulah.RSS.App.Services;
+
+public class FeedService
+{
+	private readonly HttpClient _feedClient;
+
+	// lazy test data that will be cleared if the refresh feed button is clicked and the API is running
+	private readonly List<FeedDetail> _cachedFeeds =
+	[
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+		new FeedDetail()
+		{
+			Title = "test",
+			Description = "test",
+			Location = "test"
+		},
+	];
+
+	public FeedService(HttpClient httpClient)
+	{
+		_feedClient = httpClient;
+	}
+
+	public async Task<List<FeedDetail>> FetchFeeds(bool forceRefresh = false)
+	{
+		// If we've loaded feeds before and have any in the system and we're not forcing a refresh, return whatever
+		// we returned previously
+		if (!forceRefresh && _cachedFeeds.Count > 0)
+		{
+			return _cachedFeeds;
+		}
+
+		// Otherwise if we loaded no feeds last time (regardless on if we have feeds previously saved or not), or 
+		// we're forcing a refresh, clear whatever we might have had, then load new data in
+		_cachedFeeds.Clear();
+
+		// We don't care if we have any results from this, as the endpoint is guaranteed to return a list with 0 or more
+		// details, so just add them to the cache and return
+		if (await _feedClient.GetFromJsonAsync<List<FeedDetail>>("rss/list") is { } loadedFeeds)
+		{
+			_cachedFeeds.AddRange(loadedFeeds);
+		}
+
+		return _cachedFeeds;
+	}
+}
