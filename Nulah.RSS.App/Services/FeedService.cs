338a8f2ff1cc3e484ef0d0dbc606958c91db92cf
Scott <scott.schultz@jotunga.com>
2024-08-02 17:28:29 +1000

    Update FeedService to use events for shared component notifications - Update styling a bit more - Placeholder view feed page - Update pages/components to use FeedService events a bit

--- Nulah.RSS.App/Services/FeedService.cs
+++ Nulah.RSS.App/Services/FeedService.cs
@@ -1,11 +1,14 @@
 ﻿using System.Net.Http.Json;
-using Nulah.RSS.Domain.Interfaces;
 using Nulah.RSS.Domain.Models;
 
 namespace Nulah.RSS.App.Services;
 
 public class FeedService
 {
+	public EventHandler? FeedsLoading;
+	public EventHandler<List<FeedDetail>>? FeedsLoaded;
+
+
 	private readonly HttpClient _feedClient;
 
 	// lazy test data that will be cleared if the refresh feed button is clicked and the API is running
@@ -15,43 +18,50 @@ public class FeedService
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -1
 		},
 		new FeedDetail()
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -2
 		},
 		new FeedDetail()
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -3
 		},
 		new FeedDetail()
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -4
 		},
 		new FeedDetail()
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -5
 		},
 		new FeedDetail()
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -6
 		},
 		new FeedDetail()
 		{
 			Title = "test",
 			Description = "test",
-			Location = "test"
+			Location = "test",
+			Id = -7
 		},
 	];
 
@@ -66,18 +76,39 @@ public class FeedService
 		// we returned previously
 		if (!forceRefresh && _cachedFeeds.Count > 0)
 		{
+			FeedsLoaded?.Invoke(this, _cachedFeeds);
 			return _cachedFeeds;
 		}
 
-		// Otherwise if we loaded no feeds last time (regardless on if we have feeds previously saved or not), or 
-		// we're forcing a refresh, clear whatever we might have had, then load new data in
-		_cachedFeeds.Clear();
-
-		// We don't care if we have any results from this, as the endpoint is guaranteed to return a list with 0 or more
-		// details, so just add them to the cache and return
-		if (await _feedClient.GetFromJsonAsync<List<FeedDetail>>("rss/list") is { } loadedFeeds)
+		try
 		{
-			_cachedFeeds.AddRange(loadedFeeds);
+			// Potential issue where mutliple components might call this at the same time, and invoke events multiple times.
+			// Feels like an annoying problem an I could potentially prevent this with semaphors but I'm genuinely not sure
+			// if I care too much currently.
+			// There may be a bit of multiple flickers as a result of this, but I'll deal with that when it annoys me.
+			FeedsLoading?.Invoke(this, EventArgs.Empty);
+
+			// Otherwise if we loaded no feeds last time (regardless on if we have feeds previously saved or not), or 
+			// we're forcing a refresh, clear whatever we might have had, then load new data in
+			// We don't care if we have any results from this, as the endpoint is guaranteed to return a list with 0 or more
+			// details, so just add them to the cache and return
+			if (await _feedClient.GetFromJsonAsync<List<FeedDetail>>("rss/list") is { } loadedFeeds)
+			{
+				// Clear the cache here, just incase we have an exception and want to preserve the previous state.
+				// Though if we have an exception, odds are the underlying data backend is cactus so attempting to view feeds
+				// may also be busted, but eh, I'll figure out how I want to change this later if it becomes an issue
+				_cachedFeeds.Clear();
+				_cachedFeeds.AddRange(loadedFeeds);
+			}
+		}
+		catch (Exception ex)
+		{
+			// Do something about the error here, maybe a shared modal or something but we'll need to evaluate how to handle
+			// displaying errors from multiple callers (eg, opening the app makes a call to this)
+		}
+		finally
+		{
+			FeedsLoaded?.Invoke(this, _cachedFeeds);
 		}
 
 		return _cachedFeeds;
